90454fad95fdec23c6f0566fc8fb98d19134bd4d
Vijay-948 <kvijayreddy948@gmail.com>
2024-03-10 22:49:08 +0530

    some-changes-added

--- src/Components/Register.tsx
+++ src/Components/Register.tsx
@@ -1,25 +1,86 @@
 import { FaRegUserCircle, FaLock } from "react-icons/fa";
 import '../Styles/Register.css';
+import { useEffect, useState } from "react";
+import { SignUp } from "../Service/user";
+import { error } from "console";
 
 const Register = () => {
+
+
+  const [data, setData] = useState({
+    firstName:'',
+    lastName:'',
+    email:'',
+    password:''
+  })
+
+  // useEffect(() => {
+  //   console.log(data);
+  // }, [data])
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>, property: string) => {
+    setData({...data, [property]:event.target.value})
+  }
+
+  const submitForm = (event: React.FormEvent<HTMLElement>) => {
+    event.preventDefault();
+
+    console.log(data);
+
+    SignUp(data).then((response) => {
+      console.log(response);
+      console.log("success log")
+    }).catch((error) => {
+      console.log(error)
+      console.log("error log");
+    })
+
+    
+  }
+
+
+
+
   return (
     <>
       <div className="login">
         <div className="container">
+          <span>{JSON.stringify(data)}</span>
           <form action="">
             <h1>Register</h1>
             <div className="input-box">
-              <input type="text" placeholder="FirstName" required />
+              <input 
+              type="text" 
+              placeholder="FirstName" 
+              id="firstName"
+              onChange={(event) => handleChange(event, 'firstName')}
+              value={data.firstName}
+              required />
             </div>
             <div className="input-box">
-              <input type="text" placeholder="LastName" required />
+              <input 
+              type="text" 
+              placeholder="LastName"
+              onChange={(event) => handleChange(event, 'lastName')}
+              value={data.lastName}
+              required />
             </div>
             <div className="input-box">
-              <input type="email" placeholder="Username" required />
+              <input 
+              type="email"
+              placeholder="Username"
+              onChange={(event) => handleChange(event, 'email')}
+              value={data.email}
+              required />
               <FaRegUserCircle className="icon" />
             </div>
             <div className="input-box">
-              <input type="password" placeholder="Password" required />
+              <input 
+              type="password" 
+              placeholder="Password"
+              onChange={(event) => handleChange(event, 'password')} 
+              value={data.password}
+              required />
               <FaLock className="icon" />
             </div>
 
@@ -30,7 +91,7 @@ const Register = () => {
               </label>
             </div>
 
-            <button type="submit">Register</button>
+            <button type="submit" onClick={submitForm}>Register</button>
 
             <div className="register-link">
               <p>
--- src/Service/user.tsx
+++ src/Service/user.tsx
@@ -1,9 +1,16 @@
 import { AxiosClient } from "../Common/AxiosClient";
 
-const SignUp = (user) => {
+
+interface User {
+    firstName: string,
+    lastName: string,
+    email: string,
+    password: string
+    
+}
+export const SignUp = (user: User) => {
 
     return AxiosClient.post('/api/v1/auth/register', user).then((response) => response.data)
 
 }
 
-export default SignUp;
