fd93fec8064a66264ac186fe7de8590ad4cbf295
vijay reddy <kvijayreddy948@gmail.com>
2024-03-13 17:25:03 +0530

    code

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,23 @@
 import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import logo from "./logo.svg";
 import "./App.css";
 import Home from "./Components/Home";
 import Register from "./Components/Register";
 import Login from "./Components/Login";
+import "react-toastify/dist/ReactToastify.css";
+import { ToastContainer } from "react-toastify";
 
 function App() {
   return (
     <>
-      {/* <Home /> */}
-      <Register />
-      {/* <Login /> */}
+      <BrowserRouter>
+        <ToastContainer />
+        <Routes>
+          <Route path="/" element={<Register />} />
+          <Route path="/hdf" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
