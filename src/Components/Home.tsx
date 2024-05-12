ba7bc3540b2f06af8684730b8948765284f851ae
Vijay-948 <kvijayreddy948@gmail.com>
2024-05-12 21:10:11 +0530

    changes-updated

--- src/Components/Home.tsx
+++ src/Components/Home.tsx
@@ -4,6 +4,7 @@ import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew';
 import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
 import FiberManualRecordIcon from '@mui/icons-material/FiberManualRecord';
 import { useState } from "react";
+import About from "./About";
 
 const slides = [
   {
@@ -36,28 +37,56 @@ const slides = [
 const Home = () => {
   const [currIndx, setCurrIndx] = useState(0);
 
+  const prevSlide = () => {
+    const isFirstSlide = currIndx === 0;
+    const newIndex = isFirstSlide ? slides.length - 1 : currIndx - 1;
+    setCurrIndx(newIndex);
+  }
+
+  const nextSlide = () => {
+    const isLastSlide = currIndx === slides.length - 1;
+    const newIndex = isLastSlide ? 0 : currIndx + 1;
+    setCurrIndx(newIndex);
+  }
+
+  const goToSlide = (slideIndex: any) => {
+    setCurrIndx(slideIndex);
+  }
+
 
   return(
     <>
     <Navbar />
-    <div className="max-w-[1500px] h-[780px] w-full m-auto py-16 px-20 flex mt-40 rounded-md  relative gap-15 ">
-      <div className="w-1/2 flex flex-col justify-center gap-20">
-        <div className="text-3xl font-bold mb-4">{slides[currIndx].name}</div>
-        <div className="text-lg mb-8">{slides[currIndx].description}</div>
+    <div className="max-w-[1500px] h-[780px] w-full m-auto py-16 px-4 relative group flex mt-40 rounded-md gap-15">
+      <div className="w-1/2 flex flex-col justify-center">
+        <div className="text-3xl font-bold mb-4 text-gray-500">{slides[currIndx].name}</div>
+        <div className="text-lg mb-8 text-blue-900">{slides[currIndx].description}</div>
       </div>
 
-      <div className="w-1/2 relative z-index:1  absolute inset-0" style={{ zIndex: '-1', filter: 'grayscale(100%)' }}>
-        <div className="w-full h-full rounded-2xl bg-center bg-no-repeat duration-500  absolute inset-0"
-            style={{backgroundImage: `url(${slides[0].imgUrl})`, zIndex: '0', transform: 'rotateY(20deg) rotateX(10deg)', boxShadow: '0 20px 50px rgba(0,0,255,0.5)'}}>
+      <div className="w-1/2 relative">
+        <div className="w-full h-full rounded-2xl bg-center bg-no-repeat duration-500"
+            style={{backgroundImage: `url(${slides[currIndx].imgUrl})`}}>
         </div>
-        <div className="hidden group-hover:block absolute text-blue-600">
-          <ArrowBackIosNewIcon/>
-
+        <div className="hidden group-hover:block absolute top-[50%] -translate-y-[50%] left-5 text-2xl rounded-full p-2 bg-black/20 text-white cursoir-pointer text-blue-600">
+          <ArrowBackIosNewIcon onClick={prevSlide} />
         </div>
-
+        <div className="hidden group-hover:block absolute top-[50%] -translate-y-[50%] right-5 text-2xl rounded-full p-2 bg-black/20 text-white cursoir-pointer text-blue-600">
+          <ArrowForwardIosIcon onClick={nextSlide} />
+        </div>
       </div>
-
+      <div className="flex top-4 justify-center">
+          {slides.map((slide) => (
+            <div
+               key={slide.id}
+               onClick={() => goToSlide(slide.id)}
+               className={`text-2xl cursor-pointer ${slide.id === currIndx ? 'text-blue-500' : 'text-red-500'}`}
+            >
+              <FiberManualRecordIcon />
+            </div>
+          ))}
+        </div>
     </div>
+    <About />
 
     </>
   )
